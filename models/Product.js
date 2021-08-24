c0cab6d4b63a4e3a0e38980f350adbaee6bf3f3a
reetah589 <reetah589@gmail.com>
2021-08-24 15:28:57 -0500

    product columns added

--- models/Product.js
+++ models/Product.js
@@ -12,19 +12,40 @@ Product.init(
     // define columns:
     // id, product_name, price, stock, category_id
     id: {
-      type:
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
+
     product_name: {
-      type:
+      type: DataTypes.STRING,
+      allowNull: false
     },
+
     price: {
-      type:
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
     },
+
     stock: {
-      type:
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
     },
+
     category_id: {
-      type:
+      type: DataTypes.INTEGER,
+      references: {
+        model: category,
+        key: id
+      }
     }
   },
   {
