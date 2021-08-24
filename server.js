be9cb8db60ceba841a5ccce28c404c160e28c052
reetah589 <reetah589@gmail.com>
2021-08-24 13:22:44 -0500

    finishing starter code clean up

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const routes = require('./routes');
 // import Sequence from 'mysql2/typings/mysql/lib/protocol/sequences/Sequence';
 // import sequelize connection
 
-const sequelize = require("./config/connection");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,17 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-
-sequelize
-    .authenticate()
-    .then(() => {
-      console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-      console.error('Unable to connect to the database:', err);
-    });
-    
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+
+//sequelize
+    //.authenticate()
+    //.then(() => {
+      //console.log('Connection has been established successfully.');
+    //})
+    //.catch(err => {
+      //console.error('Unable to connect to the database:', err);
+    //});
 
